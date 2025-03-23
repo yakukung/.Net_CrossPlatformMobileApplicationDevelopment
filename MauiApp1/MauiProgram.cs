7688cc90424acbd9951d3da122c69216fe4e172e
Yakukung <yakukung2003@gmail.com>
2025-03-23 20:42:33 +0700

    first commit

---
+++ MauiApp1/MauiProgram.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiApp1;
+
+public static class MauiProgram
+{
+    public static MauiApp CreateMauiApp()
+    {
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+            });
+
+        builder.Services.AddSingleton<Services.DataService>();
+        builder.Services.AddTransient<ViewModels.ProfilePageViewModel>();
+        builder.Services.AddTransient<ViewModels.HomePageViewModel>();
+
+        builder.Services.AddTransient<Views.ProfilePage>();
+        builder.Services.AddTransient<Views.HomePage>();
+
+#if DEBUG
+        builder.Logging.AddDebug();
+#endif
+
+        return builder.Build();
+    }
+}
